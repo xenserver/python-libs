ff32a73f925cbf00e412cd4f954372ed423bbd80
Simon Rowe <simon.rowe@eu.citrix.com>
2011-02-15 10:08:14 +0000

    Add directory package file

---
+++ __init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# Copyright (c) 2011 Citrix Systems, Inc. All use and distribution of this
+# copyrighted material is governed by and subject to terms and conditions
+# as licensed by Citrix Systems, Inc. All other rights reserved.
+# Xen, XenSource and XenEnterprise are either registered trademarks or
+# trademarks of Citrix Systems, Inc. in the United States and/or other 
+# countries.
+

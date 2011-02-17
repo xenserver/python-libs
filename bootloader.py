44ecafe14d1ebca81ec8c50ed41ef7a5ba9153df
Andrew Cooper <andrew.cooper3@citrix.com>
2011-02-17 13:29:24 +0000

    Lint'd bootloader.py

--- bootloader.py
+++ bootloader.py
@@ -14,13 +14,14 @@
 import os
 import os.path
 import re
-import sys
+# import sys
 import tempfile
 
-counter = 0
+COUNTER = 0
 
-class MenuEntry:
-    def __init__(self, hypervisor, hypervisor_args, kernel, kernel_args, initrd, title = None):
+class MenuEntry(object):
+    def __init__(self, hypervisor, hypervisor_args, kernel, kernel_args,
+                 initrd, title = None):
         self.hypervisor = hypervisor
         self.hypervisor_args = hypervisor_args
         self.kernel = kernel
@@ -40,8 +41,17 @@ class MenuEntry:
     def setKernelArgs(self, args):
         self.kernel_args = ' '.join(args)
     
-class Bootloader:
-    def __init__(self, src_fmt, src_file, menu = {}, menu_order = [], default = None, timeout = None, serial = None, location = None):
+class Bootloader(object):
+    def __init__(self, src_fmt, src_file, menu = None, menu_order = None,
+                 default = None, timeout = None, serial = None, 
+                 location = None):
+
+        if menu is None:
+            menu = {}
+
+        if menu_order is None:
+            menu_order = []
+
         self.src_fmt = src_fmt
         self.src_file = src_file
         self.menu = menu
@@ -79,7 +89,8 @@ class Bootloader:
                     continue
 
                 # header
-                if l.startswith('# location ') and len(els) == 3 and els[2] in ['mbr', 'partition']:
+                if (l.startswith('# location ') and len(els) == 3 and 
+                    els[2] in ['mbr', 'partition']):
                     location = els[2]
                 elif els[0] == 'serial' and len(els) > 1:
                     baud = 9600
@@ -101,16 +112,20 @@ class Bootloader:
                     if els[0] == '#':
                         title = l[1:].lstrip()
                     elif els[0] == 'append' and len(els) > 1:
-                        # els[2] contains hypervisor args, kernel, kernel args & initrd
+                        # els[2] contains hypervisor args, kernel,
+                        # kernel args & initrd
                         args = map(lambda x: x.strip(), els[2].split('---'))
                         if len(args) == 3:
                             kernel = args[1].split(None, 1)
                             if len(kernel) == 2:
-                                menu[label] = MenuEntry(els[1], args[0], kernel[0], kernel[1], args[2], title)
+                                menu[label] = MenuEntry(els[1], args[0], 
+                                                        kernel[0], kernel[1],
+                                                        args[2], title)
         finally:
             fh.close()
 
-        return cls('extlinux', src_file, menu, menu_order, default, timeout, serial, location)
+        return cls('extlinux', src_file, menu, menu_order, default, timeout,
+                   serial, location)
 
     @classmethod
     def readGrub(cls, src_file):
@@ -128,7 +143,7 @@ class Bootloader:
         kernel_args = None
 
         def create_label(title):
-            global counter
+            global COUNTER
             
             # FIXME use branding
             if title == 'XenServer':
@@ -143,8 +158,8 @@ class Bootloader:
                     return 'fallback-serial'
                 else:
                     return 'fallback'
-            counter += 1
-            return "label%d" % counter
+            COUNTER += 1
+            return "label%d" % COUNTER
 
         fh = open(src_file)
         try:
@@ -155,7 +170,8 @@ class Bootloader:
                     continue
 
                 # header
-                if l.startswith('# location ') and len(els) == 3 and els[2] in ['mbr', 'partition']:
+                if (l.startswith('# location ') and len(els) == 3 and 
+                    els[2] in ['mbr', 'partition']):
                     location = els[2]
                 elif els[0] == 'serial' and len(els) > 1:
                     port = 0
@@ -179,24 +195,29 @@ class Bootloader:
                     title = l.split(None, 1)[1]
                 elif title:
                     if els[0] == 'kernel' and len(els) > 2:
-                        hypervisor, hypervisor_args =  l.split(None, 1)[1].split(None, 1)
+                        hypervisor, hypervisor_args =  (l.split(None, 1)
+                                                        [1].split(None, 1))
                     elif els[0] == 'module' and len(els) > 1:
                         if kernel and hypervisor:
                             # second module == initrd
                             label = create_label(title)
                             menu_order.append(label)
-                            menu[label] = MenuEntry(hypervisor, hypervisor_args, kernel, kernel_args, els[1], title)
+                            menu[label] = MenuEntry(hypervisor, hypervisor_args,
+                                                    kernel, kernel_args,
+                                                    els[1], title)
                             hypervisor = None
                             kernel = None
                         else:
-                            kernel, kernel_args = l.split(None, 1)[1].split(None, 1)
+                            kernel, kernel_args = (l.split(None, 1)
+                                                   [1].split(None, 1))
             # fixup default
             if len(menu_order) > default:
                 default = menu_order[default]
         finally:
             fh.close()
 
-        return cls('grub', src_file, menu, menu_order, default, timeout, serial, location)
+        return cls('grub', src_file, menu, menu_order, default,
+                   timeout, serial, location)
 
     @classmethod
     def loadExisting(cls, root = '/'):
@@ -212,23 +233,25 @@ class Bootloader:
             fh = dst_file
         else:
             fh = open(dst_file, 'w')
-        print >>fh, "# location " + self.location
+        print >> fh, "# location " + self.location
 
         if self.serial:
-            print >>fh, "serial %d %d" % (self.serial['port'], self.serial['baud'])
+            print >> fh, "serial %d %d" % (self.serial['port'],
+                                           self.serial['baud'])
         if self.default:
-            print >>fh, "default " + self.default
-        print >>fh, "prompt 1"
+            print >> fh, "default " + self.default
+        print >> fh, "prompt 1"
         if self.timeout:
-            print >>fh, "timeout %d" % self.timeout
+            print >> fh, "timeout %d" % self.timeout
 
         for label in self.menu_order:
-            print >>fh, "\nlabel " + label
+            print >> fh, "\nlabel " + label
             m = self.menu[label]
             if m.title:
-                print >>fh, "  # " + m.title
-            print >>fh, "  kernel mboot.c32"
-            print >>fh, "  append %s %s --- %s %s --- %s" % (m.hypervisor, m.hypervisor_args, m.kernel, m.kernel_args, m.initrd)
+                print >> fh, "  # " + m.title
+            print >> fh, "  kernel mboot.c32"
+            print >> fh, "  append %s %s --- %s %s --- %s" % (m.hypervisor, 
+                     m.hypervisor_args, m.kernel, m.kernel_args, m.initrd)
         if not hasattr(dst_file, 'name'):
             fh.close()
 
@@ -237,27 +260,28 @@ class Bootloader:
             fh = dst_file
         else:
             fh = open(dst_file, 'w')
-        print >>fh, "# location " + self.location
+        print >> fh, "# location " + self.location
 
         if self.serial:
-            print >>fh, "serial --unit=%d --speed=%s" % (self.serial['port'], self.serial['baud'])
-            print >>fh, "terminal --timeout=10 console serial"
+            print >> fh, "serial --unit=%d --speed=%s" % (self.serial['port'],
+                                                          self.serial['baud'])
+            print >> fh, "terminal --timeout=10 console serial"
         else:
-            print >>fh, "terminal console"
+            print >> fh, "terminal console"
         if self.default:
             for i in range(len(self.menu_order)):
                 if self.menu_order[i] == self.default:
-                    print >>fh, "default %d" % i
+                    print >> fh, "default %d" % i
                     break
         if self.timeout:
-            print >>fh, "timeout %d" % (self.timeout / 10)
+            print >> fh, "timeout %d" % (self.timeout / 10)
 
         for label in self.menu_order:
             m = self.menu[label]
-            print >>fh, "\ntitle " + m.title
-            print >>fh, "   kernel " + m.hypervisor + " " + m.hypervisor_args
-            print >>fh, "   module " + m.kernel + " " + m.kernel_args
-            print >>fh, "   module " + m.initrd
+            print >> fh, "\ntitle " + m.title
+            print >> fh, "   kernel " + m.hypervisor + " " + m.hypervisor_args
+            print >> fh, "   module " + m.kernel + " " + m.kernel_args
+            print >> fh, "   module " + m.initrd
         if not hasattr(dst_file, 'name'):
             fh.close()
 

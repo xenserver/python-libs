fac4b78c886e0832b7866cc1d0c522af86be56e2
Andrew Cooper <andrew.cooper3@citrix.com>
2011-03-14 15:50:57 +0000

    Inserted GPL header and bumped version number for mac.py

--- mac.py
+++ mac.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
+# Copyright (c) 2011 Citrix Systems, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; version 2.1 only. with the special
+# exception on linking described in file LICENSE.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
 
 """
 Mac address object for manipulation and comparison
 """
 
-__version__ = "0.1"
+__version__ = "1.0"
 __author__  = "Andrew Cooper <andrew.cooper3@citrix.com>"
 
 import re

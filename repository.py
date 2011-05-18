854ca4a35c03afde602b8befdd2aa7a1dcd3efbd
Simon Rowe <simon.rowe@eu.citrix.com>
2011-05-18 13:29:02 +0100

    Extract repo metadata

--- repository.py
+++ repository.py
@@ -11,21 +11,94 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 
+import md5
 import os.path
+import xml.dom.minidom
 
-#import xcl.accessor as accessor
-#import xcp.accessor
+import xcp.version as version
+import xcp.xmlunwrap as xmlunwrap
+
+class NoRepository(Exception):
+    pass
+
+class RepoFormatError(Exception):
+    pass
 
 class Repository(object):
     REPOSITORY_FILENAME = "XS-REPOSITORY"
     PKGDATA_FILENAME = "XS-PACKAGES"
     REPOLIST_FILENAME = "XS-REPOSITORY-LIST"
 
+    XS_MAIN_IDENT = "xs:main"
+
+    OPER_MAP = {'eq': ' = ', 'ne': ' != ', 'lt': ' < ', 'gt': ' > ', 'le': ' <= ', 'ge': ' >= '}
+
     def __init__(self, access, base):
         (
             self.accessor,
             self.base
         ) = (access, base)
+        self._md5 = md5.new()
+        self.requires = []
+
+        access.start()
+
+        try:
+            repofile = access.openAddress(os.path.join(base, self.REPOSITORY_FILENAME))
+        except Exception, e:
+            access.finish()
+            raise NoRepository, e
+        self._parse_repofile(repofile)
+        repofile.close()
+
+        access.finish()
+
+    def __repr__(self):
+        return "<Repository '%s', version '%s'>" % (self.identifier, self.product_version)
+
+    def _parse_repofile(self, repofile):
+        """ Parse repository data -- get repository identifier and name. """
+        
+        repofile_contents = repofile.read()
+        repofile.close()
+
+        # update md5sum for repo
+        self._md5.update(repofile_contents)
+
+        # build xml doc object
+        try:
+            xmldoc = xml.dom.minidom.parseString(repofile_contents)
+        except:
+            raise RepoFormatError, "%s not in XML" % self.REPOSITORY_FILENAME
+
+        try:
+            repo_node = xmlunwrap.getElementsByTagName(xmldoc, ['repository'], mandatory = True)
+            desc_node = xmlunwrap.getElementsByTagName(xmldoc, ['description'], mandatory = True)
+            originator = xmlunwrap.getStrAttribute(repo_node[0], ['originator'], mandatory = True)
+            name = xmlunwrap.getStrAttribute(repo_node[0], ['name'], mandatory = True)
+            product = xmlunwrap.getStrAttribute(repo_node[0], ['product'], mandatory = True)
+            version_s = xmlunwrap.getStrAttribute(repo_node[0], ['version'], mandatory = True)
+            build = xmlunwrap.getStrAttribute(repo_node[0], ['build'], None)
+            description = xmlunwrap.getText(desc_node[0])
+
+            for req_node in xmlunwrap.getElementsByTagName(xmldoc, ['requires']):
+                req = {}
+                for attr in ['originator', 'name', 'test', 'version', 'build']:
+                    req[attr] = xmlunwrap.getStrAttribute(req_node, [attr])
+                if req['build'] == '':
+                    del req['build']
+                assert req['test'] in self.OPER_MAP
+                self.requires.append(req)
+        except:
+            raise RepoFormatError, "%s format error" % self.REPOSITORY_FILENAME
+
+        self.identifier = "%s:%s" % (originator, name)
+        self.name = description
+        self.product_brand = product
+        ver_str = version_s
+        if build:
+            ver_str += '-'+build
+        self.product_version = version.Version.from_string(ver_str)
 
     @classmethod
     def isRepo(cls, access, base):

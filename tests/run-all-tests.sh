2e089cc342990526ff6c0da6e9c8b359a539f7ab
Andrew Cooper <andrew.cooper3@citrix.com>
2012-03-01 15:49:05 +0000

    Add wrapper scripts for unit tests to bind mount 'xcp'

---
+++ tests/run-all-tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SITE_PACKAGE_NAME=xcp
+
+if [[ ! -d "xcp" ]]; then
+    mkdir $SITE_PACKAGE_NAME
+fi
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Must be run as root to bind mount"
+    exit 2
+fi
+
+
+mount --bind ../../python-libs.hg $SITE_PACKAGE_NAME
+
+if [[ $? -ne 0 ]]; then
+    echo "Mount failed"
+    exit 3
+fi
+
+for f in $(find . \( -path "./xcp" -prune \) -o -name "test*.py" -print)
+do
+    echo "Running $f"
+    python $f
+done
+
+umount $SITE_PACKAGE_NAME
+rmdir $SITE_PACKAGE_NAME --ignore-fail-on-non-empty

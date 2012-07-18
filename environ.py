1e9c252a1c32e7753affddaa66bf8d62972b9e2b
Simon Rowe <simon.rowe@eu.citrix.com>
2012-07-18 09:16:11 +0100

    CA-86355: add method to read inventory

--- environ.py
+++ environ.py
@@ -12,8 +12,16 @@
 # GNU Lesser General Public License for more details.
 
 import os
+import os.path
 
 EXTRA_SCRIPTS_DIR = '/mnt'
 
 def installerRunning():
     return os.environ.get('XS_INSTALLATION', '0') != '0'
+
+def readInventory(root = '/'):
+    fh = open(os.path.join(root, 'etc/xensource-inventory'))
+    d = dict(map(lambda x: [x[:x.find('=')], x[x.find('=')+1:].strip().strip("'")], fh))
+    fh.close()
+
+    return d

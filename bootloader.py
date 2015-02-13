6b2935813693824d6cd5f9355f1eddf527ffb26d
Ross Lagerwall <ross.lagerwall@citrix.com>
2015-02-13 10:06:35 +0000

    CP-10445: Add grub2 support
    Since grub2 configuration files are effectively shell scripts, parsing
    the configuration is done on a best effort basis while attempting to
    maintain any unrecognized lines. Loading and writing out Ubuntu's grub
    configuration results in whitespace changes only.
    
    The implementation also attempts to support two kinds of default settings:
    - Setting default to a number, like 0 or 1.
    - Setting default to a string, representing the menu entry title.

--- bootloader.py
+++ bootloader.py
@@ -29,6 +29,7 @@ import os
 import os.path
 import re
 import tempfile
+import copy
 
 COUNTER = 0
 
@@ -285,8 +286,134 @@ class Bootloader(object):
                    timeout, serial, location)
 
     @classmethod
+    def readGrub2(cls, src_file):
+        menu = {}
+        menu_order = []
+        default = 0
+        timeout = None
+        serial = None
+        title = None
+        hypervisor = None
+        hypervisor_args = None
+        kernel = None
+        kernel_args = None
+        initrd = None
+        menu_entry_extra = None
+        menu_entry_contents = []
+        boilerplate = []
+        boilerplates = []
+
+        def create_label(title):
+            global COUNTER
+
+            # FIXME use branding
+            if title == 'XenServer':
+                return 'xe'
+
+            if title.endswith('(Serial)'):
+                return 'xe-serial'
+            if title.endswith('Safe Mode'):
+                return 'safe'
+            if ' / ' in title:
+                if '(Serial,' in title:
+                    return 'fallback-serial'
+                else:
+                    return 'fallback'
+            COUNTER += 1
+            return "label%d" % COUNTER
+
+        fh = open(src_file)
+        try:
+            for line in fh:
+                l = line.strip()
+                menu_match = re.match("menuentry ['\"]([^']*)['\"](.*){", l)
+
+                # Only parse unindented default and timeout lines to prevent
+                # changing these lines in if statements.
+                if l.startswith('set default=') and l == line.rstrip():
+                    default = l.split('=')[1]
+                    match = re.match("['\"](.*)['\"]$", default)
+                    if match:
+                        default = match.group(1)
+                elif l.startswith('set timeout=') and l == line.rstrip():
+                    timeout = int(l.split('=')[1]) * 10
+                elif l.startswith('serial'):
+                    match = re.match("serial --unit=(\d+) --speed=(\d+)", l)
+                    if match:
+                        serial = {
+                            'port': int(match.group(1)),
+                            'baud': match.group(2)
+                        }
+                elif l.startswith('terminal_'):
+                    pass
+                elif menu_match:
+                    title = menu_match.group(1)
+                    menu_entry_extra = menu_match.group(2)
+                    boilerplates.append(boilerplate)
+                    boilerplate = []
+                elif title:
+                    if l.startswith("multiboot"):
+                        hypervisor, hypervisor_args = (l.split(None, 1)
+                                                       [1].split(None, 1))
+                    elif l.startswith("module"):
+                        if kernel:
+                            initrd = l.split(None, 1)[1]
+                        else:
+                            kernel, kernel_args = (l.split(None, 1)
+                                                   [1].split(None, 1))
+                    elif l.startswith("linux"):
+                        kernel, kernel_args = (l.split(None, 1)
+                                               [1].split(None, 1))
+                    elif l.startswith("initrd"):
+                        initrd = l.split(None, 1)[1]
+                    elif l == "}":
+                        label = create_label(title)
+                        menu_order.append(label)
+                        menu[label] = MenuEntry(hypervisor = hypervisor,
+                                                hypervisor_args = hypervisor_args,
+                                                kernel = kernel,
+                                                kernel_args = kernel_args,
+                                                initrd = initrd, title = title)
+                        menu[label].extra = menu_entry_extra
+                        menu[label].contents = menu_entry_contents
+
+                        title = None
+                        hypervisor = None
+                        hypervisor_args = None
+                        kernel = None
+                        kernel_args = None
+                        initrd = None
+                        menu_entry_extra = None
+                        menu_entry_contents = []
+
+                    else:
+                        menu_entry_contents.append(line.rstrip())
+                else:
+                    boilerplate.append(line.rstrip())
+
+            # Try parse default as an index into the menu_order list.
+            # If this fails, it is probably a string, so leave it unchanged.
+            try:
+                default = menu_order[int(default)]
+            except ValueError, KeyError:
+                pass
+        finally:
+            fh.close()
+
+        bootloader = cls('grub2', src_file, menu, menu_order, default,
+                         timeout, serial, None)
+        bootloader.boilerplate = boilerplates
+        return bootloader
+
+    @classmethod
     def loadExisting(cls, root = '/'):
-        if os.path.exists(os.path.join(root, "boot/extlinux.conf")):
+        if os.path.exists(os.path.join(root, "boot/efi/EFI/xenserver/grub.cfg")):
+            return cls.readGrub2(os.path.join(root, "boot/efi/EFI/xenserver/grub.cfg"))
+        elif os.path.exists(os.path.join(root, "boot/grub/grub.cfg")):
+            return cls.readGrub2(os.path.join(root, "boot/grub/grub.cfg"))
+        elif os.path.exists(os.path.join(root, "boot/grub2/grub.cfg")):
+            return cls.readGrub2(os.path.join(root, "boot/grub2/grub.cfg"))
+        elif os.path.exists(os.path.join(root, "boot/extlinux.conf")):
             return cls.readExtLinux(os.path.join(root, "boot/extlinux.conf"))
         elif os.path.exists(os.path.join(root, "boot/grub/menu.lst")):
             return cls.readGrub(os.path.join(root, "boot/grub/menu.lst"))
@@ -369,6 +496,67 @@ class Bootloader(object):
         if not hasattr(dst_file, 'name'):
             fh.close()
 
+    def writeGrub2(self, dst_file = None):
+        if hasattr(dst_file, 'name'):
+            fh = dst_file
+        else:
+            fh = open(dst_file, 'w')
+
+        if self.serial:
+            print >> fh, "serial --unit=%d --speed=%s" % (self.serial['port'],
+                                                          self.serial['baud'])
+            print >> fh, "terminal_input serial console"
+            print >> fh, "terminal_output serial"
+        if self.default:
+            for i in range(len(self.menu_order)):
+                if self.menu_order[i] == self.default:
+                    print >> fh, "set default=%d" % i
+                    break
+            else:
+                print >> fh, "set default='%s'" % str(self.default)
+        if self.timeout:
+            print >> fh, "set timeout=%d" % (self.timeout / 10)
+
+        self.boilerplate = getattr(self, 'boilerplate', [])
+        self.boilerplate.reverse()
+
+        for label in self.menu_order:
+            m = self.menu[label]
+
+            if self.boilerplate:
+                text = self.boilerplate.pop()
+                if text:
+                    print >> fh, "\n".join(text)
+
+            extra = ' '
+            try:
+                extra = m.extra
+            except AttributeError:
+                pass
+            print >> fh, "menuentry '%s'%s{" % (m.title, extra)
+
+            try:
+                contents = "\n".join(m.contents)
+                if contents:
+                    print >> fh, contents
+            except AttributeError:
+                pass
+
+            if m.hypervisor:
+                print >> fh, "\tmultiboot %s %s" % (m.hypervisor, m.hypervisor_args)
+                if m.kernel:
+                    print >> fh, "\tmodule %s %s" % (m.kernel, m.kernel_args)
+                if m.initrd:
+                    print >> fh, "\tmodule %s" % m.initrd
+            else:
+                if m.kernel:
+                    print >> fh, "\tlinux %s %s" % (m.kernel, m.kernel_args)
+                if m.initrd:
+                    print >> fh, "\tinitrd %s" % m.initrd
+            print >> fh, "}"
+        if not hasattr(dst_file, 'name'):
+            fh.close()
+
     def commit(self, dst_file = None):
         if not dst_file:
             dst_file = self.src_file
@@ -380,6 +568,8 @@ class Bootloader(object):
             self.writeExtLinux(tmp_file)
         elif self.src_fmt == 'grub':
             self.writeGrub(tmp_file)
+        elif self.src_fmt == 'grub2':
+            self.writeGrub2(tmp_file)
 
         # atomically replace destination file
         os.close(fd)
@@ -403,7 +593,10 @@ class Bootloader(object):
 
             for k, v in b.menu.items():
                 if v.kernel == old_kernel_link:
-                    backup.append((k+old_ver, MenuEntry(**v.__dict__)))
+                    c = copy.deepcopy(v)
+                    if c.title:
+                        c.title += " (%s)" % old_ver
+                    backup.append((k+old_ver, c))
                     v.kernel = kernel_link_name
                     v.initrd = initrd_link_name
 

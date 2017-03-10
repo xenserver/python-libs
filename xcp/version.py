23c550a8623db5ac5d45898843f8f0e2a0f05a5b
Ross Lagerwall <ross.lagerwall@citrix.com>
2017-03-10 09:49:37 +0000

    CA-246490: version: Change the build number to a build identifier
    The build number is now a build identifier, so change the code to work
    with this. Drop the build suffix handling.  The build identifier is not
    useful for version comparisons, so don't use it in comparisons.
    
    This results in a slight change in behavior in the host installer.
    Previously, it would not be possible to upgrade from a later build to an
    earlier build of the same platform version. After this change, it would
    be possible. This shouldn't be an issue as within a platform version are
    only used for testing.

--- xcp/version.py
+++ xcp/version.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Copyright (c) 2013, Citrix Inc.
+Copyright (c) 2013, 2017 Citrix Inc.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -28,10 +28,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """version - version comparison methods"""
 
 class Version:
-    def __init__(self, ver, build = None, build_suf = None):
+    def __init__(self, ver, build = None):
         self.ver = ver
         self.build = build
-        self.build_suf = build_suf
 
     @staticmethod
     def intify(x):
@@ -43,13 +42,12 @@ class Version:
     def from_string(cls, ver_str):
         """ Create an object instance from a string conforming to:
 
-        p.q. ... y.z[-b[s]]
+        p.q. ... y.z[-b]
 
         where:
 
         p.q. ... y.z are arcs
-        b is a build number
-        s is a build suffix
+        b is a build identifier
 
         an arc is one of:
 
@@ -57,37 +55,25 @@ class Version:
         a sequence of subarcs, s-t-u"""
 
         build = None
-        build_suf = None
 
         ver = map(cls.intify, ver_str.split('.'))
 
-        if type(ver[-1]) is str and '-' in ver[-1] and ver[-1][-1].isalpha():
-            ver_el, build_str = ver[-1].rsplit('-', 1)
+        if type(ver[-1]) is str and '-' in ver[-1]:
+            ver_el, build = ver[-1].split('-', 1)
             ver[-1] = cls.intify(ver_el)
 
-            if build_str:
-                if not build_str.isdigit():
-                    build_suf = build_str[-1]
-                    build_str = build_str[:-1]
-                build = int(build_str)
-
-        return cls(ver, build, build_suf)
+        return cls(ver, build)
 
     def ver_as_string(self):
         return '.'.join(map(str, self.ver))
 
     def build_as_string(self):
-        val = ''
-        if self.build:
-            val += str(self.build)
-        if self.build_suf:
-            val += self.build_suf
-        return val
+        return self.build if self.build else ''
 
     def __str__(self):
         build = self.build_as_string()
         if build != '':
-            return self.ver_as_string() + '-' + self.build_as_string()
+            return self.ver_as_string() + '-' + build
         return self.ver_as_string()
 
     #************************************************************
@@ -96,27 +82,13 @@ class Version:
     #
     # The version is always compared.
     #
-    # If rhs has a build it is compared. If lhs has no build it evaluates
-    # to -1.
-    #
-    # Build suffix is ignored.
+    # Build identifiers are ignored.
     #
     #************************************************************
 
     @classmethod
     def arc_cmp(cls, l, r):
-        if type(l) is int and type(r) is int:
-            return (l-r)
-        elif type(l) is str and '-' in l:
-            if type(r) is str and '-' in r:
-                return cls.ver_cmp(map(cls.intify, l.split('-')),
-                                   map(cls.intify, r.split('-')))
-            elif type(r) is int:
-                return cls.ver_cmp(map(cls.intify, l.split('-')), [r])
-        elif type(l) is int:
-            return cls.ver_cmp([l], map(cls.intify, r.split('-')))
-        else:
-            raise RuntimeError, "Invalid arc types"
+        return l - r
 
     @classmethod
     def ver_cmp(cls, l, r):
@@ -132,30 +104,20 @@ class Version:
         # equal to this point, down to list length
         return (len(l) - len(r))
 
-    def ver_build_cmp(self, v):
-        l = self.ver[:]
-        r = v.ver[:]
-
-        if v.build != None:
-            l.append(self.build == None and -1 or self.build)
-            r.append(v.build)
-
-        return self.ver_cmp(l, r)
-
     def __eq__(self, v):
-        return self.ver_build_cmp(v) == 0
+        return self.ver_cmp(self.ver, v.ver) == 0
 
     def __ne__(self, v):
-        return self.ver_build_cmp(v) != 0
+        return self.ver_cmp(self.ver, v.ver) != 0
 
     def __lt__(self, v):
-        return self.ver_build_cmp(v) < 0
+        return self.ver_cmp(self.ver, v.ver) < 0
 
     def __gt__(self, v):
-        return self.ver_build_cmp(v) > 0
+        return self.ver_cmp(self.ver, v.ver) > 0
 
     def __le__(self, v):
-        return self.ver_build_cmp(v) <= 0
+        return self.ver_cmp(self.ver, v.ver) <= 0
 
     def __ge__(self, v):
-        return self.ver_build_cmp(v) >= 0
+        return self.ver_cmp(self.ver, v.ver) >= 0

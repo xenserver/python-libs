xenserver/python-libs

commit f38078b80925e54cbbb432414dc7a177b24d6cd4

CP-28832: Enable the use of an index after PCI bus location
Some NICs expose multiple ethernet devices for a single PCI location, meaning
there is confusion about the physical names and no way to distinguish between
them other than MAC address.  Auditing a large environment to obtain MAC
addresses to feed into the host installer (e.g. using map_netdev) is not
viable, therefore this change introduces an index (ordered by MAC) to the PCI
bus location to uniquely identify a NIC

xcp/net/ifrename/dynamic.py
@@ -47,6 +47,7 @@ except ImportError:
 
 from xcp.logger import LOG
 from xcp.net.ifrename.macpci import MACPCI
+from xcp.pci import pci_sbdfi_to_nic
 from os.path import exists as pathexists
 
 SAVE_HEADER = (
@@ -210,18 +211,15 @@ class DynamicRules(object):
 
             elif method == "pci":
 
-                for nic in state:
-                    if nic.pci == value:
-                        try:
-                            rule = MACPCI(nic.mac, nic.pci, tname=target)
-                        except Exception, e:
-                            LOG.warning("Error creating rule: %s" % (e,))
-                            continue
-                        self.rules.append(rule)
-                        break
-                else:
-                    LOG.warning("No NIC found with a PCI ID of '%s' for the "
-                                "%s dynamic rule" % (value, target))
+                nic = pci_sbdfi_to_nic(value, state)
+
+                try:
+                    rule = MACPCI(nic.mac, nic.pci, tname=target)
+                except Exception, e:
+                    LOG.warning("Error creating rule: %s" % (e,))
+                    continue
+                self.rules.append(rule)
+
                 continue
 
             elif method == "label":

xcp/net/ifrename/static.py
@@ -34,7 +34,7 @@ Rules are of the form:
 target name must be in the form eth*
 id methods are:
   mac: value should be the mac address of a device (e.g. DE:AD:C0:DE:00:00)
-  pci: value should be the pci bus location of the device (e.g. 0000:01:01.1)
+  pci: value should be the pci bus location of the device, optionally with an index (e.g. 0000:01:01.1[0])
   ppn: value should be the result of the biosdevname physical naming policy of a device (e.g. p1p1)
   label: value should be the SMBios label of a device (for SMBios 2.6 or above)
 
@@ -50,8 +50,9 @@ import re
 from xcp.logger import LOG
 from xcp.net.mac import VALID_COLON_MAC as VALID_MAC
 from xcp.net.ifrename.macpci import MACPCI
-from xcp.pci import VALID_SBDF as VALID_PCI
+from xcp.pci import VALID_SBDFI as VALID_PCI
 from os.path import exists as pathexists
+from xcp.pci import pci_sbdfi_to_nic
 
 VALID_LINE = re.compile(
     r"^\s*(?P<target>eth\d+)"         # <target name>
@@ -70,7 +71,7 @@ SAVE_HEADER = """# Static rules.  Autogenerated by the installer from the answer
 # target name must be in the form eth*
 # id methods are:
 #   mac: value should be the mac address of a device (e.g. DE:AD:C0:DE:00:00)
-#   pci: value should be the pci bus location of the device (e.g. 0000:01:01.1)
+#   pci: value should be the pci bus location of the device (e.g. 0000:01:01.1[0])
 #   ppn: value should be the result of the biosdevname physical naming policy of a device (e.g. p1p1)
 #   label: value should be the SMBios label of a device (for SMBios 2.6 or above)
 
@@ -261,19 +262,15 @@ class StaticRules(object):
                 continue
 
             elif method == "pci":
+                nic = pci_sbdfi_to_nic(value, state)
 
-                for nic in state:
-                    if nic.pci == value:
-                        try:
-                            rule = MACPCI(nic.mac, nic.pci, tname=target)
-                        except Exception, e:
-                            LOG.warning("Error creating rule: %s" % (e,))
-                            continue
-                        self.rules.append(rule)
-                        break
-                else:
-                    LOG.warning("No NIC found with a PCI ID of '%s' for the "
-                                "%s static rule" % (value, target))
+                try:
+                    rule = MACPCI(nic.mac, nic.pci, tname=target)
+                except Exception, e:
+                    LOG.warning("Error creating rule: %s" % (e,))
+                    continue
+                self.rules.append(rule)
+
                 continue
 
             elif method == "label":

tests/test_ifrename_dynamic.py
@@ -130,6 +130,22 @@ class TestGenerate(unittest.TestCase):
         # being generated
         self.assertEqual(dr.rules, [])
 
+    def test_pci_matching_invert(self):
+
+        dr = DynamicRules()
+        dr.formulae = { "eth0" : ("pci", "0000:04:00.0[1]"),
+                        "eth1" : ("pci", "0000:04:00.0")
+                        }
+
+        dr.generate([MACPCI("c8:cb:b8:d3:0c:ce", "0000:04:00.0", kname="eth0",
+                            ppn="em1", label=""),
+                     MACPCI("c8:cb:b8:d3:0c:cf", "0000:04:00.0", kname="eth1",
+                            ppn="", label="")])
+
+        self.assertEqual(dr.rules,[
+                MACPCI("c8:cb:b8:d3:0c:ce", "0000:04:00.0", tname="eth1"),
+                MACPCI("c8:cb:b8:d3:0c:cf", "0000:04:00.0", tname="eth0")
+                ])
 
 
 class TestSave(unittest.TestCase):

tests/test_ifrename_static.py
@@ -106,6 +106,15 @@ class TestLoadAndParse(unittest.TestCase):
         self.assertEqual(sr.formulae, {"eth0": ("pci", "0000:00:00.1")})
         self.assertEqual(sr.rules, [])
 
+    def test_single_pci_0index(self):
+
+        fd = StringIO.StringIO('eth0:pci="0000:00:00.1[0]"')
+        sr = StaticRules(fd = fd)
+
+        self.assertTrue(sr.load_and_parse())
+        self.assertEqual(sr.formulae, {"eth0": ("pci", "0000:00:00.1[0]")})
+        self.assertEqual(sr.rules, [])
+
     def test_single_invalid_ppn(self):
 
         fd = StringIO.StringIO('eth0:ppn="baz"')
@@ -196,6 +205,15 @@ class TestLoadAndParseGuess(unittest.TestCase):
 
         self.assertTrue(sr.load_and_parse())
         self.assertEqual(sr.formulae, {"eth0": ("pci", "0000:00:00.0")})
+        self.assertEqual(sr.rules, [])
+
+    def test_single_pci_index(self):
+
+        fd = StringIO.StringIO("eth0=0000:00:00.0[1]")
+        sr = StaticRules(fd = fd)
+
+        self.assertTrue(sr.load_and_parse())
+        self.assertEqual(sr.formulae, {"eth0": ("pci", "0000:00:00.0[1]")})
         self.assertEqual(sr.rules, [])
 
     def test_single_ppn_embedded(self):
@@ -335,6 +353,70 @@ class TestGenerate(unittest.TestCase):
         # The quirks test should kick in and prevent any ppn rules from
         # being generated
         self.assertEqual(sr.rules, [])
+
+
+class TestMultiplePCI(unittest.TestCase):
+
+    def setUp(self):
+        self.logbuf = StringIO.StringIO()
+        openLog(self.logbuf, logging.NOTSET)
+        self.state = [
+            MACPCI("c8:cb:b8:d3:0c:ca", "0000:03:00.0", kname="eth0",
+                   ppn="em1", label=""),
+            MACPCI("c8:cb:b8:d3:0c:cb", "0000:03:00.1", kname="eth1",
+                   ppn="em2", label=""),
+            MACPCI("c8:cb:b8:d3:0c:ce", "0000:04:00.0", kname="eth2",
+                   ppn="em3", label=""),
+            MACPCI("c8:cb:b8:d3:0c:cf", "0000:04:00.0", kname="eth3",
+                   ppn="", label="")
+            ]
+
+    def tearDown(self):
+
+        closeLogs()
+        self.logbuf.close()
+
+    def test_pci_matching(self):
+
+        fd = StringIO.StringIO('eth0:pci="0000:04:00.0"\n'
+                               'eth1:pci="0000:04:00.0[1]"')
+        sr = StaticRules(fd = fd)
+        self.assertTrue(sr.load_and_parse())
+
+        sr.generate(self.state)
+
+        self.assertEqual(sr.rules,[
+                MACPCI("c8:cb:b8:d3:0c:cf", "0000:04:00.0", tname="eth1"),
+                MACPCI("c8:cb:b8:d3:0c:ce", "0000:04:00.0", tname="eth0")
+                ])
+
+    def test_pci_matching_invert(self):
+
+        fd = StringIO.StringIO('eth0:pci="0000:04:00.0[1]"\n'
+                               'eth1:pci="0000:04:00.0[0]"')
+        sr = StaticRules(fd = fd)
+        self.assertTrue(sr.load_and_parse())
+
+        sr.generate(self.state)
+
+        self.assertEqual(sr.rules,[
+                MACPCI("c8:cb:b8:d3:0c:ce", "0000:04:00.0", tname="eth1"),
+                MACPCI("c8:cb:b8:d3:0c:cf", "0000:04:00.0", tname="eth0")
+                ])
+
+    def test_pci_matching_mixed(self):
+
+        fd = StringIO.StringIO('eth0:ppn="em3"\n'
+                               'eth1:pci="0000:04:00.0[1]"')
+        sr = StaticRules(fd = fd)
+        self.assertTrue(sr.load_and_parse())
+
+        sr.generate(self.state)
+
+        self.assertEqual(sr.rules,[
+                MACPCI("c8:cb:b8:d3:0c:cf", "0000:04:00.0", tname="eth0"),
+                MACPCI("c8:cb:b8:d3:0c:ce", "0000:04:00.0", tname="eth1")
+                ])
 
 
 class TestSave(unittest.TestCase):

xcp/pci.py
@@ -29,11 +29,19 @@ import os.path
 import subprocess
 import re
 
-VALID_SBDF = re.compile(
-    r"^(?:(?P<segment> [\da-dA-F]{4}):)?" # Segment (optional)
-    "     (?P<bus>     [\da-fA-F]{2}):"   # Bus
-    "     (?P<device>  [\da-fA-F]{2})\."  # Device
-    "     (?P<function>[\da-fA-F])$"      # Function"
+_SBDF = (r"(?:(?P<segment> [\da-dA-F]{4}):)?" # Segment (optional)
+         "     (?P<bus>     [\da-fA-F]{2}):"   # Bus
+         "     (?P<device>  [\da-fA-F]{2})\."  # Device
+         "     (?P<function>[\da-fA-F])"       # Function
+         )
+
+# Don't change the meaning of VALID_SBDF as some parties may be using it
+VALID_SBDF = re.compile(r"^%s$" % _SBDF, re.X)
+
+VALID_SBDFI = re.compile(
+    r"^(?P<sbdf>%s)"
+    "  (?:[[](?P<index>[\d]{1,2})[]])?$"   # Index (optional)
+    % _SBDF
     , re.X)
 
 
@@ -60,10 +68,11 @@ class PCI(object):
         self.bus = -1
         self.device = -1
         self.function = -1
+        self.index = -1
 
         if isinstance(addr, (str, unicode)):
 
-            res = VALID_SBDF.match(addr)
+            res = VALID_SBDFI.match(addr)
             if res:
                 groups = res.groupdict()
 
@@ -87,11 +96,16 @@ class PCI(object):
                     raise ValueError("Function '%d' out of range 0 <= device "
                                      "< 8" % (self.function,))
 
-                self.integer = int( self.segment      << 16 |
-                                     self.bus      << 8  |
-                                     self.device   << 3  |
-                                     self.function
-                                     )
+                if "index" in groups and groups["index"] is not None:
+                    self.index = int(groups["index"])
+                else:
+                    self.index = 0
+
+                self.integer = (int(self.segment   << 16 |
+                                    self.bus       << 8  |
+                                    self.device    << 3  |
+                                    self.function) << 8  |
+                                self.index)
                 return
 
             raise ValueError("Unrecognised PCI address '%s'" % addr)
@@ -101,8 +115,9 @@ class PCI(object):
 
 
     def __str__(self):
-        return "%04x:%02x:%02x.%1x" % (self.segment, self.bus,
-                                       self.device, self.function)
+        pci_sbdf = "%04x:%02x:%02x.%1x" % (self.segment, self.bus,
+                                           self.device, self.function)
+        return "%s[%d]" % (pci_sbdf, self.index)
 
     def __repr__(self):
         return "<PCI %s>" % (self,)
@@ -282,6 +297,25 @@ class PCIDevices(object):
 
         return filter(lambda x: x != dev and slot(x) == slot(dev),
                       self.devs.keys())
+
+
+def pci_sbdfi_to_nic(sbdfi, nics):
+    match = VALID_SBDFI.match(sbdfi)
+
+    index = 0
+    if 'index' in match.groupdict():
+        index_str = match.group("index")
+        if index_str is not None:
+            index = int(index_str)
+    value = match.group("sbdf")
+
+    matching_nics = [nic for nic in nics if nic.pci == value]
+    matching_nics.sort(key=lambda nic: nic.mac)
+
+    if index >= len(matching_nics):
+        raise Exception("Insufficient NICs with PCI SBDF %s (Found %d, wanted at least %d)" % (value, len(matching_nics), index))
+
+    return matching_nics[index]
 
 
 if __name__ == "__main__":

commit 27f79fa727190b78f5c22b3b890a9ecdb6346043

CA-343343: Handle PCI rules when device is missing
If the device for a PCI rule cannot be found, log a message rather than
just exiting with an exception. This restores the previous behaviour.

Fixes: f38078b80925 ("CP-28832: Enable the use of an index after PCI bus location")

xcp/net/ifrename/dynamic.py
@@ -210,10 +210,8 @@ class DynamicRules(object):
                 continue
 
             elif method == "pci":
-
-                nic = pci_sbdfi_to_nic(value, state)
-
                 try:
+                    nic = pci_sbdfi_to_nic(value, state)
                     rule = MACPCI(nic.mac, nic.pci, tname=target)
                 except Exception, e:
                     LOG.warning("Error creating rule: %s" % (e,))

xcp/net/ifrename/static.py
@@ -262,9 +262,8 @@ class StaticRules(object):
                 continue
 
             elif method == "pci":
-                nic = pci_sbdfi_to_nic(value, state)
-
                 try:
+                    nic = pci_sbdfi_to_nic(value, state)
                     rule = MACPCI(nic.mac, nic.pci, tname=target)
                 except Exception, e:
                     LOG.warning("Error creating rule: %s" % (e,))

tests/test_ifrename_dynamic.py
@@ -147,6 +147,19 @@ class TestGenerate(unittest.TestCase):
                 MACPCI("c8:cb:b8:d3:0c:cf", "0000:04:00.0", tname="eth0")
                 ])
 
+    def test_pci_missing(self):
+
+        dr = DynamicRules()
+        dr.formulae = {"eth0" : ("pci", "0000:04:00.0"),
+                       "eth1" : ("pci", "0000:05:00.0")}
+
+        dr.generate([MACPCI("c8:cb:b8:d3:0c:ce", "0000:04:00.0", kname="eth0",
+                            ppn="em1", label="")])
+
+        self.assertEqual(dr.rules, [
+                MACPCI("c8:cb:b8:d3:0c:ce", "0000:04:00.0", tname="eth0")
+                ])
+
 
 class TestSave(unittest.TestCase):
 

tests/test_ifrename_static.py
@@ -418,6 +418,19 @@ class TestMultiplePCI(unittest.TestCase):
                 MACPCI("c8:cb:b8:d3:0c:ce", "0000:04:00.0", tname="eth1")
                 ])
 
+    def test_pci_missing(self):
+
+        fd = StringIO.StringIO('eth0:pci="0000:03:00.0"\n'
+                               'eth4:pci="0000:05:00.0"')
+        sr = StaticRules(fd = fd)
+        self.assertTrue(sr.load_and_parse())
+
+        sr.generate(self.state)
+
+        self.assertEqual(sr.rules,[
+                MACPCI("c8:cb:b8:d3:0c:ca", "0000:03:00.0", tname="eth0")
+                ])
+
 
 class TestSave(unittest.TestCase):
 
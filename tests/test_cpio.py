182e4d87a6916b1a9ece2a930022d88a06cc9db2
Yann Dirson <yann.dirson@vates.fr>
2022-07-13 18:25:47 +0200

    tests: rework test_cpio so it can be called by pytest
    Signed-off-by: Yann Dirson <yann.dirson@vates.fr>

--- tests/test_cpio.py
+++ tests/test_cpio.py
@@ -31,34 +31,32 @@ def writeRandomFile(fn, size, start='', add='a'):
     f.close()
 
 
-doXZ = False
-md5data = ''
-
 def check_call(cmd):
     r = subprocess.call(cmd, shell=True)
     if r != 0:
         raise Exception('error executing command')
 
-# create some archive from scratch
-def prepare():
-    global doXZ, md5data
-    shutil.rmtree('archive', True)
-    os.mkdir('archive')
-    writeRandomFile('archive/data', 10491)
-    md5data = md5(open('archive/data').read()).hexdigest()
-    check_call("find archive | cpio -o -H newc > archive.cpio")
-    check_call("gzip -c < archive.cpio > archive.cpio.gz")
-    check_call("bzip2 -c < archive.cpio > archive.cpio.bz2")
-    try:
-        import lzma
-        doXZ = subprocess.call("xz --check=crc32 --lzma2=dict=1MiB < archive.cpio > archive.cpio.xz", shell=True) == 0
-    except:
-        doXZ = False
-
-def cleanup():
-    check_call("rm -rf archive archive.cpio*")
-
 class TestCpio(unittest.TestCase):
+    def setUp(self):
+        self.doXZ = False
+        self.md5data = ''
+
+        # create some archive from scratch
+        shutil.rmtree('archive', True)
+        os.mkdir('archive')
+        writeRandomFile('archive/data', 10491)
+        self.md5data = md5(open('archive/data').read()).hexdigest()
+        check_call("find archive | cpio -o -H newc > archive.cpio")
+        check_call("gzip -c < archive.cpio > archive.cpio.gz")
+        check_call("bzip2 -c < archive.cpio > archive.cpio.bz2")
+        try:
+            import lzma
+            self.doXZ = subprocess.call("xz --check=crc32 --lzma2=dict=1MiB < archive.cpio > archive.cpio.xz", shell=True) == 0
+        except:
+            self.doXZ = False
+
+    def tearDown(self):
+        check_call("rm -rf archive archive.cpio*")
 
     # TODO check with file (like 'r:*')
     # TODO use cat to check properly for pipes
@@ -69,7 +67,7 @@ class TestCpio(unittest.TestCase):
             if f.isfile():
                 data = arc.extractfile(f).read()
                 self.assertEqual(len(data), f.size)
-                self.assertEqual(md5data, md5(data).hexdigest())
+                self.assertEqual(self.md5data, md5(data).hexdigest())
                 found = True
         self.assertTrue(found)
 
@@ -104,15 +102,10 @@ class TestCpio(unittest.TestCase):
         self.doArchive('archive.cpio.bz2', 'bz2')
 
     def test_xz(self):
-        if not doXZ:
+        if not self.doXZ:
             return
         print 'Running test for XZ'
         self.doArchive('archive.cpio.xz', 'xz')
 
 if __name__ == "__main__":
-    try:
-        prepare()
-        unittest.main()
-    finally:
-        cleanup()
-
+    unittest.main()

cc0121e5f7b790bf607a864e6ab619f877e72d35
Yann Dirson <yann.dirson@vates.fr>
2022-07-25 11:15:27 +0200

    Coverage: new test_bootloader
    We may want to do some real checks on the output, this first version
    only exercises the code.
    
    Includes ad-hoc testing of write/read methods for grub/extlinux from a
    grub2 configuration, for lack of representative versions of those
    deprecated config files.
    
    Ad-hoc test for extlinux reveals inconsistent typing, and is marked as
    failing for now:
    
                    print("serial %d %d" % (self.serial['port'],
     >                                       self.serial['baud']), file=fh)
     E               TypeError: %d format: a number is required, not str
    
    Coverage change:
    xcp.bootloader: 0% -> 59.1%%
    total: 45.2% -> 53.4%
    
    Signed-off-by: Yann Dirson <yann.dirson@vates.fr>

---
+++ tests/test_bootloader.py
@@ -0,0 +1,41 @@
+import unittest
+import os
+import subprocess
+from tempfile import NamedTemporaryFile
+
+from xcp.bootloader import Bootloader
+
+class TestBootloader(unittest.TestCase):
+    def test_grub2(self):
+        bl = Bootloader.readGrub2("tests/data/grub.cfg")
+        with NamedTemporaryFile("w") as temp:
+            bl.writeGrub2(temp.name)
+            # get a diff
+            proc = subprocess.Popen(["diff", "tests/data/grub.cfg", temp.name],
+                                    stdout = subprocess.PIPE,
+                                    universal_newlines=True)
+        for line in proc.stdout:
+            # FIXME: check is entirely ad-hoc, should we have a diff at all ?
+            self.assertRegexpMatches(line, r"^(5a6,13$|>)")
+
+        proc.stdout.close()
+        proc.wait()
+
+
+class TestBootloaderAdHoc(unittest.TestCase):
+    def setUp(self):
+        self.bl = Bootloader.readGrub2("tests/data/grub.cfg")
+
+    def test_grub(self):
+        with NamedTemporaryFile("w", delete=False) as temp:
+            self.bl.writeGrub(temp)
+        bl2 = Bootloader.readGrub(temp.name)
+        os.unlink(temp.name)
+
+    @unittest.expectedFailure
+    def test_extlinux(self):
+        # FIXME that one triggers a format error, real inconsistency bug ?
+        with NamedTemporaryFile("w", delete=False) as temp:
+            self.bl.writeExtLinux(temp)
+        bl2 = Bootloader.readExtLinux(temp.name)
+        os.unlink(temp.name)

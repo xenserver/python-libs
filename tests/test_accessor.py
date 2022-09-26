b004cab48ca683d29848f1285592bad259d349e9
Yann Dirson <yann.dirson@vates.fr>
2022-09-26 14:18:59 +0200

    test: use parametrized tests
    Testing several accessor classes causes code duplication, which can be
    avoided with help from the `parametrized` package (unfortunately, `pytest`
    support cannot be used together with `unittest`).
    
    Not a big deal right now, but starts becoming painful when adding new tests
    or testing other Accessor classes.
    
    Signed-off-by: Yann Dirson <yann.dirson@vates.fr>

--- tests/test_accessor.py
+++ tests/test_accessor.py
@@ -1,19 +1,13 @@
 import unittest
+from parameterized import parameterized_class
 
 import xcp.accessor
 
+@parameterized_class([{"url": "file://tests/data/repo/"},
+                      {"url": "https://updates.xcp-ng.org/netinstall/8.2.1"}])
 class TestAccessor(unittest.TestCase):
-    def test_http(self):
-        #raise unittest.SkipTest("comment out if you really mean it")
-        a = xcp.accessor.createAccessor("https://updates.xcp-ng.org/netinstall/8.2.1", True)
-        a.start()
-        self.assertTrue(a.access('.treeinfo'))
-        self.assertFalse(a.access('no_such_file'))
-        self.assertEqual(a.lastError, 404)
-        a.finish()
-
-    def test_file(self):
-        a = xcp.accessor.createAccessor("file://tests/data/repo/", True)
+    def test_access(self):
+        a = xcp.accessor.createAccessor(self.url, True)
         a.start()
         self.assertTrue(a.access('.treeinfo'))
         self.assertFalse(a.access('no_such_file'))

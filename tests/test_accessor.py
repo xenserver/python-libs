961bb79f4046b5173d48aabac41500916f3a36e7
Yann Dirson <yann.dirson@vates.fr>
2022-09-26 14:19:10 +0200

    WIP test_accessor: check for I/O on binary files
    This test uses the same kind of I/O (file copy) that prepare_host_upgrade.py
    does.
    
    FIXME: the copy cannot proceed this way in python3

--- tests/test_accessor.py
+++ tests/test_accessor.py
@@ -1,4 +1,7 @@
 import unittest
+import hashlib
+from tempfile import NamedTemporaryFile
+
 from parameterized import parameterized_class
 
 import xcp.accessor
@@ -12,4 +15,24 @@ class TestAccessor(unittest.TestCase):
         self.assertTrue(a.access('.treeinfo'))
         self.assertFalse(a.access('no_such_file'))
         self.assertEqual(a.lastError, 404)
+        a.finish()
+
+    def test_file_binfile(self):
+        BINFILE = "boot/isolinux/mboot.c32"
+        a = xcp.accessor.createAccessor(self.url, True)
+        a.start()
+        self.assertTrue(a.access(BINFILE))
+        inf = a.openAddress(BINFILE)
+        with NamedTemporaryFile("w") as outf:
+            outf.writelines(inf)
+            outf.flush()
+            hasher = hashlib.md5()
+            with open(outf.name, "rb") as bincontents:
+                while True:
+                    data = bincontents.read()
+                    if not data: # EOF
+                        break
+                    hasher.update(data)
+                csum = hasher.hexdigest()
+                self.assertEqual(csum, "eab52cebc3723863432dc672360f6dac")
         a.finish()

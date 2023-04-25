5b8eec72427ccbe63983a2806e1d732407d74781
Bernhard Kaindl <bernhard.kaindl@cloud.com>
2023-04-25 18:53:20 +0200

    xcp.repository: switch from ConfigParser to configparser
    This is supposed to be just a module renaming to conform to PEP8, see
    https://docs.python.org/3/whatsnew/3.0.html#library-changes
    
    The SafeConfigParser class has been renamed to ConfigParser in Python
    3.2, and backported as addon package.  The `readfp` method now
    triggers a deprecation warning to replace it with `read_file`.
    
    Originally authored by Yann Dirson,
    Updated by Bernhard Kaindl to no longer need the WIP marker,
    to unblock the Python3 work, with more tests to be added.
    
    Co-authored-by: Yann Dirson <yann.dirson@vates.fr>
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@cloud.com>

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -6,7 +6,7 @@ from xcp.version import Version
 
 class TestRepository(unittest.TestCase):
     def test_http(self):
-        raise unittest.SkipTest("comment out if you really mean it")
+        #raise unittest.SkipTest("comment out if you really mean it")
         a = xcp.accessor.createAccessor("https://updates.xcp-ng.org/netinstall/8.2.1", True)
         repo_ver = repository.BaseRepository.getRepoVer(a)
         self.assertEqual(repo_ver, Version([3, 2, 1]))

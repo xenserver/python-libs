4d8fd6321bc60086931fdc4611cca70c2350f13c
Bernhard Kaindl <bernhard.kaindl@cloud.com>
2023-04-25 18:19:12 +0200

    test_accessor.py: Test reading binary data from http and file
    Add and enable a parameterized test to read binary data
    from a file- and an HTTP based accessor.
    
    Originally authored by Yann Dirson. Extracted, refactored
    and squashed for a single considated and concise commit by me.
    
    Co-authored-by: Yann Dirson <yann.dirson@vates.fr>
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@cloud.com>

--- tests/test_accessor.py
+++ tests/test_accessor.py
@@ -1,21 +1,46 @@
 import unittest
+import hashlib
+from tempfile import NamedTemporaryFile
+
+from parameterized import parameterized_class
 
 import xcp.accessor
 
+@parameterized_class([{"url": "file://tests/data/repo/"},
+                      {"url": "https://updates.xcp-ng.org/netinstall/8.2.1"}])
 class TestAccessor(unittest.TestCase):
-    def test_http(self):
-        #raise unittest.SkipTest("comment out if you really mean it")
-        a = xcp.accessor.createAccessor("https://updates.xcp-ng.org/netinstall/8.2.1", True)
-        a.start()
-        self.assertTrue(a.access('.treeinfo'))
+    url = ""
+
+    def setup_accessor(self, arg0):
+        result = xcp.accessor.createAccessor(self.url, True)
+        result.start()
+        self.assertTrue(result.access(arg0))
+        return result
+
+    def test_access(self):
+        a = self.setup_accessor('.treeinfo')
         self.assertFalse(a.access('no_such_file'))
         self.assertEqual(a.lastError, 404)
         a.finish()
 
-    def test_file(self):
-        a = xcp.accessor.createAccessor("file://tests/data/repo/", True)
-        a.start()
-        self.assertTrue(a.access('.treeinfo'))
-        self.assertFalse(a.access('no_such_file'))
-        self.assertEqual(a.lastError, 404)
+    def test_file_binfile(self):
+        binary_file_in_repo = "boot/isolinux/mboot.c32"
+        a = self.setup_accessor(binary_file_in_repo)
+        inf = a.openAddress(binary_file_in_repo)
+        with NamedTemporaryFile("wb") as outf:
+            while True:
+                data = inf.read()
+                if not data: # EOF
+                    break
+                outf.write(data)
+            outf.flush()
+            hasher = hashlib.md5()
+            with open(outf.name, "rb") as bincontents:
+                while True:
+                    data = bincontents.read()
+                    if not data: # EOF
+                        break
+                    hasher.update(data)
+                csum = hasher.hexdigest()
+                self.assertEqual(csum, "eab52cebc3723863432dc672360f6dac")
         a.finish()

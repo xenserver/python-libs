b23911cc8f2c7a343cc2f3425bd74d7b0404addf
Bernhard Kaindl <bernhard.kaindl@cloud.com>
2023-05-05 14:15:44 +0200

    xcp.xmlunwrap: Cover getIntAttribute() for fixing a pytype warning
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@cloud.com>

--- tests/test_xmlunwrap.py
+++ tests/test_xmlunwrap.py
@@ -2,11 +2,11 @@ import unittest
 import xml.dom.minidom
 
 from xcp.xmlunwrap import (getElementsByTagName, getText, getMapAttribute,
-                           getStrAttribute, XmlUnwrapError)
+                           getStrAttribute, getIntAttribute, XmlUnwrapError)
 
 class TestXmlUnwrap(unittest.TestCase):
     def setUp(self):
-        a_text = """<installation mode='test'>
+        a_text = """<installation mode='test' integer='1'>
         <fred>text1</fred>
         <fred>text2</fred>
         </installation>"""
@@ -20,17 +20,25 @@ class TestXmlUnwrap(unittest.TestCase):
                           for el in getElementsByTagName(self.top_el, ["fred"])],
                          ["text1", "text2"])
 
+        # Test xcp.xmlunwrap.getIntAttribute()
+        self.assertEqual(getIntAttribute(self.top_el, ["integer"], 5), 1)
+        self.assertEqual(getIntAttribute(self.top_el, ["noexist"], 5), 5)
+        with self.assertRaises(XmlUnwrapError):
+            getIntAttribute(self.top_el, ["nonexisting-attribute"])
+
+        # Test xcp.xmlunwrap.getMapAttribute()
         x = getMapAttribute(self.top_el, ["mode"], [('test', 42), ('stuff', 77)])
         self.assertEqual(x, 42)
         x = getMapAttribute(self.top_el, ["made"], [('test', 42), ('stuff', 77)],
                             default='stuff')
         self.assertEqual(x, 77)
 
+        # Test xcp.xmlunwrap.getIntAttribute()
         x = getStrAttribute(self.top_el, ["mode"])
         self.assertEqual(x, "test")
         x = getStrAttribute(self.top_el, ["made"])
         self.assertEqual(x, "")
-        x = getStrAttribute(self.top_el, ["made"], None)
+        x = getStrAttribute(self.top_el, ["made"], None)  # pyright: ignore
         self.assertEqual(x, None)
 
         with self.assertRaises(XmlUnwrapError):

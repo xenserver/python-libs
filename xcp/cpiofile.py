1f6fedba55c4f6e0bd53c0e36ebd4d5e24d2d5c6
Bernhard Kaindl <bernhard.kaindl@cloud.com>
2023-06-12 16:46:58 +0200

    xcp/cpiofile.py: Fix pyright warnings in the _CMPProxy class
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@cloud.com>

--- xcp/cpiofile.py
+++ xcp/cpiofile.py
@@ -503,8 +503,8 @@ class _CMPProxy(object):
         self.fileobj = fileobj
         self.mode = mode
         self.cmpobj = None
-        self.buf = None
-        self.pos = None
+        self.buf = b""
+        self.pos = 0
 
     def read(self, size):
         b = [self.buf]
@@ -512,6 +512,7 @@ class _CMPProxy(object):
         while x < size:
             try:
                 raw = self.fileobj.read(self.blocksize)
+                assert self.cmpobj
                 data = self.cmpobj.decompress(raw)
                 b.append(data)
             except EOFError:
@@ -538,11 +539,13 @@ class _CMPProxy(object):
 
     def write(self, data):
         self.pos += len(data)
+        assert self.cmpobj
         raw = self.cmpobj.compress(data)
         self.fileobj.write(raw)
 
     def close(self):
         if self.mode == "w":
+            assert self.cmpobj
             raw = self.cmpobj.flush()
             self.fileobj.write(raw)
         if not isinstance(self.fileobj, io.BytesIO):  # BytesIO() would free the archive on close()

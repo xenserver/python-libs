86180c07b71ba78b6bb200c74760eaac354a0882
Bernhard Kaindl <bernhard.kaindl@cloud.com>
2023-06-30 18:12:29 +0200

    xcp/compat.py: open_defaults_for_utf8_text() Default to text mode
    For xcp.cmd.runCmd() to be compatible with host-installer's runCmd,
    make `mode="r"` the default for runCmd(all commands in host-installer
    return text, not binary data to the caller)
    
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@cloud.com>

--- xcp/compat.py
+++ xcp/compat.py
@@ -52,9 +52,7 @@ def open_defaults_for_utf8_text(args, kwargs):
         mode = args[0]
     if sys.version_info < (3, 0):
         return mode, other_kwargs
-    if not mode or not isinstance(mode, str):
-        raise ValueError("The mode argument is required! r for text, rb for binary")
     if sys.version_info >= (3, 0) and "b" not in mode:
         kwargs.setdefault("encoding", "utf-8")
         kwargs.setdefault("errors", "replace")
-    return mode, other_kwargs
+    return mode or "r", other_kwargs

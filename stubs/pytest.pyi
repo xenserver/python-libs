e52bfbd65f5925fa4784641d1319ef4452959c7c
Bernhard Kaindl <bernhard.kaindl@cloud.com>
2024-11-15 12:00:00 +0100

    Fix pylint and 2nd half of the pyright warnings in unit tests
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@cloud.com>

--- stubs/pytest.pyi
+++
@@ -1,8 +0,0 @@
-# pylint: disable=reimported,no-name-in-module,unused-import,function-redefined.redefined-builtin
-from _pytest.python_api import raises
-from _typeshed import Incomplete as fixture
-from _typeshed import Incomplete as mark
-
-def skip(msg: str = "", *, allow_module_level: bool = False): ...
-
-__all__ = ["mark", "fixture", "skip", "raises"]

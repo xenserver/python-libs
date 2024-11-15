38baca73e76c7c355f47ceaa02fa4b2158119889
Bernhard Kaindl <bernhard.kaindl@cloud.com>
2024-11-15 12:00:00 +0100

    `tests/test_logger.py`: Use `pyfakefs`: don't create a file in the host
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@cloud.com>

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -10,8 +10,16 @@ from xcp.compat import open_utf8
 from xcp.logger import openLog
 
 
-def test_openLog_mock_open():
-    """Cover xcp.logger.openLog.open_with_codec_handling and check the arguments used for open()"""
+def test_openLog_mock_open(fs):
+    # type(FakeFilesystem) -> None
+    """
+    - Covers xcp.logger.openLog.open_with_codec_handling and
+    - checks the arguments it uses for open()
+
+    Because needs to call openLog() which creates a logfile, this test uses
+    the pytest pyfakefs fixture 'fs' which wraps creating it in a virtual fs.
+    With it, this tests does not create a log file on the filesystem of the host.
+    """
     fh = StringIO()
     with patch("xcp.compat.open", mock_open()) as open_mock:
         open_mock.return_value = fh
